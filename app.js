a6122c7622ad601e8a387028fce3e0f70b15d361
Joseph Byron Lane <24197141@modelo.edu.mx>
2024-12-16 23:30:09 -0600

    added error checking for db conneciton

--- app.js
+++ app.js
@@ -4,6 +4,9 @@ var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
 
+const { sequelize, Service } = require('./models');
+
+
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
 
@@ -19,6 +22,7 @@ app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
 
+//routes
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
 
