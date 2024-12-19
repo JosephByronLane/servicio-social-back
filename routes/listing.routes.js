1f31976d7c828e23b9bd8077184f60f06751e943
Joseph Byron Lane <24197141@modelo.edu.mx>
2024-12-19 00:36:05 -0600

    added service validator

--- routes/listing.routes.js
+++ routes/listing.routes.js
@@ -4,9 +4,10 @@ const { validateHouse } = require('../validators/house.validator');
 const validateResult = require('../middleware/resultValidator.middleware');
 const { validateListing } = require('../validators/listing.validator');
 const { validateOwner } = require('../validators/ownerValidator');
+const { validateServices } = require('../validators/service.validator');
 const router = express.Router();
 
-router.post('/', validateHouse('house.'), validateListing('listing.'), validateOwner('owner.'), validateResult,  createListing);
+router.post('/', validateHouse('house.'), validateListing('listing.'), validateOwner('owner.'), validateServices(''), validateResult,  createListing);
 
 
 module.exports = router;
