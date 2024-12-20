3c7424861e8209f84dc98b01f46068879271e415
Joseph Byron Lane <24197141@modelo.edu.mx>
2024-12-19 18:37:59 -0600

    forgot imports on house router

--- routes/house.routes.js
+++ routes/house.routes.js
@@ -1,17 +1,20 @@
-const { router } = require("../app");
+const express = require('express');
 const { getHouses, createHouse, getHousesByOwnerId, getHousesByOwnerEmail, deleteHouseById, getHouseById } = require("../controllers/house.controller");
 const validateResult = require("../middleware/resultValidator.middleware");
 const { validateEmail } = require("../validators/email.validator");
 const { validateHouse } = require("../validators/house.validator");
 const { validateId } = require("../validators/id.validator");
+const router = express.Router();
 
 
 
 router.get('/', getHouses);
-router.get('/owner/:id', validateId, validateResult, getHousesByOwnerId)
-router.get('/email/:email', validateEmail, validateResult, getHousesByOwnerEmail)
-router.get('/:id', validateId, validateResult, getHouseById)
+router.get('/owner/:id', validateId, validateResult, getHousesByOwnerId);
+router.get('/email/:email', validateEmail, validateResult, getHousesByOwnerEmail);
+router.get('/:id', validateId, validateResult, getHouseById);
 
-router.post('/', validateHouse, validateResult ,createHouse)
+router.post('/', validateHouse, validateResult ,createHouse);
 
-router.delete('/:id', validateId, validateResult, deleteHouseById)  
+router.delete('/:id', validateId, validateResult, deleteHouseById);
+
+module.exports = router;
